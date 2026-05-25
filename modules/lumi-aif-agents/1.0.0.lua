a4282aa595a68bdf32ce7150c216db35ad672899
Mitja Sainio <mitjasai@csc.fi>
2026-05-25 15:18:31 +0300

    Disable Slurm usage
    Disable Slurm usage for now. This could be implemented in a more secure
    manner using an STDIO MCP server shipped with the container.

--- modules/lumi-aif-agents/1.0.0.lua
+++ modules/lumi-aif-agents/1.0.0.lua
@@ -23,17 +23,10 @@ local bindPaths = {
     -- The current working directory is mounted by default,
     -- which should be enough for a coding agent.
     --
+    -- Additional bind mounts:
+    --
     -- Software installed by LUST and local organizations
     "/appl",
-    -- Slurm configuration
-    "/etc/slurm",
-    -- Frequently used Slurm commands
-    "/usr/bin/sacct", "/usr/bin/sbatch", "/usr/bin/scancel",
-    "/usr/bin/sinfo", "/usr/bin/squeue", "/usr/bin/srun",
-    -- Libraries required for using Slurm
-    "/usr/lib64/libmunge.so.2", "/usr/lib64/slurm",
-    -- Variable data dirs required for using Slurm
-    "/var/run/munge", "/var/spool/slurmd",
 }
 setenv("SINGULARITY_BIND", table.concat(bindPaths, ","))
 
